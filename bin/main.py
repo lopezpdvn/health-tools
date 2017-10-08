ce38c9eefebee1dfd4c0f89f311936c4da3bff32
Pedro Ivan Lopez <m@pedroivanlopez.com>
2017-10-08 15:57:31 -0500

    Added 2017 function

--- bin/main.py
+++ bin/main.py
@@ -1,8 +1,15 @@
 from glob import iglob
 import json
 
+FILES_FP_PATTERN = 'data/2017/*json'
+
 data = []
 
-for month in sorted(iglob('data/2017/*json')):
-    with open(month) as f:
-        data.extend(json.load(f))
+def get_2017_data(fp_pattern=FILES_FP_PATTERN):
+    data = []
+    for month in sorted(iglob('data/2017/*json')):
+        with open(month) as f:
+            data.extend(json.load(f))
+    return data
+
+data = get_2017_data()
