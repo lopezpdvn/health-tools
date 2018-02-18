b68570ae16ea2c1f08a216d842ebfddb70f9226b
Pedro Ivan Lopez <m@pedroivanlopez.com>
2018-02-18 14:32:33 -0600

    Added brief report of sample averages

--- bin/main.py
+++ bin/main.py
@@ -1,5 +1,6 @@
 from glob import iglob
 from datetime import datetime, timedelta
+from pprint import pprint
 import json
 
 from timeman import DEFAULT_DATETIME_FMT
@@ -35,5 +36,13 @@ def get_average_from_then(prop_retriever, start):
             prop_retriever(x) for x in data if x[DATETIME_KEY] >= start)
     nrange_samples = len(range_samples)
     return (sum(range_samples) / nrange_samples, nrange_samples)
+
+def get_recent_averages(prop_retriever, sample_range):
+    return tuple(
+        (i, sum(prop_retriever(x) for x in data[-i:]) / i)
+        for i in sample_range)
+
+def main_rpt(prop_retriever, sample_range):
+    pprint(get_recent_averages(prop_retriever, sample_range))
 
 data = get_data()
